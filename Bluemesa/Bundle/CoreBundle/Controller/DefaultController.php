5d97725f97dfbc6832c049081e8713e0ed72c112
Radoslaw Ejsmont <radoslaw@ejsmont.net>
2015-10-25 11:03:13 +0100

    Initialized bundle structure

---
+++ Bluemesa/Bundle/CoreBundle/Controller/DefaultController.php
@@ -0,0 +1,28 @@
+<?php
+
+/*
+ * This file is part of the BluemesaCoreBundle.
+ * 
+ * Copyright (c) 2016 BlueMesa LabDB Contributors <labdb@bluemesa.eu>
+ * 
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Bluemesa\Bundle\CoreBundle\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
+
+class DefaultController extends Controller
+{
+    /**
+     * @Route("/hello/{name}")
+     * @Template()
+     */
+    public function indexAction($name)
+    {
+        return array('name' => $name);
+    }
+}
